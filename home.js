9b2901e8087f7161eecefbab5465b912b33dde2e
AI-Codemaster <ai5445359@gmail.com>
2025-10-16 20:31:46 +0600

    Payoo Mobile Bank

--- home.js
+++ home.js
@@ -5,12 +5,39 @@ document
     const bank = document.getElementById('bank').value;
     const accountNumber = document.getElementById('account-number').value;
     const amount = parseInt(document.getElementById('add-amount').value);
-    const pin = document.getElementById('add-pin').value;
+    const pin = parseInt(document.getElementById('add-pin').value);
     const availableBalance = parseInt(
       document.getElementById('available-balance').innerText
     );
+
+    if (accountNumber.length !== 11) {
+      alert('Please Provide A Valid Number');
+      return;
+    }
+
+    if (pin !== 1234) {
+      alert('Please Provide A Valid Pin Number');
+      return;
+    }
+
     const totalNewAvailableBalance = amount + availableBalance;
 
     document.getElementById('available-balance').innerText =
       totalNewAvailableBalance;
   });
+
+// Add Money Button Event Handler
+
+document.getElementById('addMoneyBtn').addEventListener('click', function () {
+  document.getElementById('cashOutSection').style.display = 'none';
+
+  document.getElementById('addMoneySection').style.display = 'block';
+});
+
+// CashOut Button Event Handler
+
+document.getElementById('cashOutBtn').addEventListener('click', function () {
+  document.getElementById('addMoneySection').style.display = 'none';
+
+  document.getElementById('cashOutSection').style.display = 'block';
+});
